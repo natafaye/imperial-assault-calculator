e62b80cd387fb9998ff5cde8f9ed728b79c10d40
Natalie Childs <nata.faye@gmail.com>
2023-05-04 13:17:13 -0700

    Made enter submit forms

--- src/components/CustomDataPicker/abilities/AbilityForm.js
+++ src/components/CustomDataPicker/abilities/AbilityForm.js
@@ -10,27 +10,30 @@ export default function AbilityForm({ idPrefix, value, defaultValue, onSave, onC
         setFormData(formData.map((num, i) => (i === property) ? newValue : num))
     }
 
-    const onSaveClick = () => {
+    const onSaveClick = (event) => {
+        event.preventDefault()
         onSave(formData.map(num => parseInt(num) || 0))
     }
 
     return (
-        <Stack direction="horizontal" gap={1} className="flex-wrap border border-secondary rounded p-2 justify-content-center">
-            { renderFormLayoutGroups({ formData, onUpdate, idPrefix }).map((group, index, groups) => (
-                <div className="d-flex flex-wrap" key={index}>
-                    { group }
-                    { index === groups.length - 1 && (
-                        <>
-                            <Button variant="outline-success" size="sm" onClick={onSaveClick} className="ms-2 me-1">
-                                <FontAwesomeIcon icon={faCheck} title="Save" />
-                            </Button>
-                            <Button variant="outline-warning" size="sm" onClick={onCancel}>
-                                <FontAwesomeIcon icon={faXmark} title="Cancel" />
-                            </Button>
-                        </>
-                    )}
-                </div>
-            )) }
-        </Stack>
+        <form>
+            <Stack direction="horizontal" gap={1} className="flex-wrap border border-secondary rounded p-2 justify-content-center">
+                { renderFormLayoutGroups({ formData, onUpdate, idPrefix }).map((group, index, groups) => (
+                    <div className="d-flex flex-wrap" key={index}>
+                        { group }
+                        { index === groups.length - 1 && (
+                            <>
+                                <Button variant="outline-success" size="sm" onClick={onSaveClick} type="submit" className="ms-2 me-1">
+                                    <FontAwesomeIcon icon={faCheck} title="Save" />
+                                </Button>
+                                <Button variant="outline-warning" size="sm" onClick={onCancel} type="button">
+                                    <FontAwesomeIcon icon={faXmark} title="Cancel" />
+                                </Button>
+                            </>
+                        )}
+                    </div>
+                )) }
+            </Stack>
+        </form>
     )
 }
